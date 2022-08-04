e4bf0a9e41da63bcd850d866a92f237bfb25d3b3
Muskan Sonkar <87746861+Muskan-090@users.noreply.github.com>
2022-08-04 13:49:56 +0530

    Update settings.py

--- NlPs/settings.py
+++ NlPs/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
